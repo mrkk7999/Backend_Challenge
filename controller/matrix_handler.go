5711fc9c84f517b5409fff4341e88cf0c60abb9f
Kiran Kshirsagar <mrkk7999@gmail.com>
2025-03-07 01:15:38 +0530

    handler function

---
+++ controller/matrix_handler.go
@@ -0,0 +1,62 @@
+package controller
+
+import (
+	"backend_challenge/utils"
+	"encoding/csv"
+	"fmt"
+	"net/http"
+)
+
+// Generic function to handle requests
+func (c *Controller) handleRequest(w http.ResponseWriter, r *http.Request, operation func([][]int) interface{}) {
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, fmt.Sprintf("File error: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	records, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("CSV parsing error: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	matrix, err := utils.ParseCSV(records)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Matrix error: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	result := operation(matrix)
+	fmt.Fprint(w, result)
+}
+
+func (c *Controller) Echo(w http.ResponseWriter, r *http.Request) {
+	c.handleRequest(w, r, func(matrix [][]int) interface{} {
+		return c.service.Echo(matrix)
+	})
+}
+func (c *Controller) Invert(w http.ResponseWriter, r *http.Request) {
+	c.handleRequest(w, r, func(matrix [][]int) interface{} {
+		return c.service.Invert(matrix)
+	})
+}
+
+func (c *Controller) Flatten(w http.ResponseWriter, r *http.Request) {
+	c.handleRequest(w, r, func(matrix [][]int) interface{} {
+		return c.service.Flatten(matrix)
+	})
+}
+
+func (c *Controller) Sum(w http.ResponseWriter, r *http.Request) {
+	c.handleRequest(w, r, func(matrix [][]int) interface{} {
+		return c.service.Sum(matrix)
+	})
+}
+
+func (c *Controller) Multiply(w http.ResponseWriter, r *http.Request) {
+	c.handleRequest(w, r, func(matrix [][]int) interface{} {
+		return c.service.Multiply(matrix)
+	})
+}
